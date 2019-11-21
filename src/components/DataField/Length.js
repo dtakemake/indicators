1b477b327b525116c2096677f3166da7127750e6
YurySamakhvalau <dev@takemake.co>
2019-11-22 00:16:12 +0300

    Добавил в пользовательские компоненты поля для ввода данных(рост, вес)

--- src/components/DataField/Length.js
+++ src/components/DataField/Length.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Form } from 'react-bootstrap'
 
 const Length = React.forwardRef(({ length, setLength, unit }, ref) => {
 
-  return <span ref={ ref }>Длина - { length } { unit }</span>
+  const _onChange = ({ currentTarget }) => {
+    
+     // написать проверку на введенное значение
+    setLength(parseInt(currentTarget.value, 10))
+  }
+
+  return (
+    <Form.Group controlId="formTest" ref={ ref }>
+      <Form.Label className="font-weight-bold small mb-1">Рост</Form.Label>
+      <Form.Control type="text" placeholder="" value={ length } onChange={ e => _onChange(e) } />
+      <Form.Text className="text-muted">
+        { unit }
+      </Form.Text>
+    </Form.Group>
+  )
 })
 
 export default React.memo(Length)
--- src/components/DataField/Weight.js
+++ src/components/DataField/Weight.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Form } from 'react-bootstrap'
 
 const Weight = React.forwardRef(({ weight, setWeight, unit }, ref) => {
 
-  return <span ref={ ref }>Вес { weight } { unit }</span>
+  const _onChange = ({ currentTarget }) => {
+    
+    // написать проверку на введенное значение
+    setWeight(parseInt(currentTarget.value, 10))
+  }
+
+  return (
+    <Form.Group controlId="formTest" ref={ ref }>
+      <Form.Label className="font-weight-bold small mb-1">Вес</Form.Label>
+      <Form.Control type="text" placeholder="" value={ weight } onChange={ e => _onChange(e) } />
+      <Form.Text className="text-muted">
+        { unit }
+      </Form.Text>
+    </Form.Group>
+  )
 })
 
 export default React.memo(Weight)
